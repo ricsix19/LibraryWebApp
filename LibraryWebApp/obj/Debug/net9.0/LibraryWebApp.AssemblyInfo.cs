7ce83f7e11068256903c6ebec4fe6e05c908369d
ricsix19 <richardladanyi30@gmail.com>
2025-05-07 21:40:13 +0200

    Changed home page text.

--- LibraryWebApp/obj/Debug/net9.0/LibraryWebApp.AssemblyInfo.cs
+++ LibraryWebApp/obj/Debug/net9.0/LibraryWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+191c570bf4dcdb2ed25c6dfab59c8e4581089d50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b546246c072a95d0095a1f151ecd7d0bad4391b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
