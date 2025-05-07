26e4241b6517cac49ebcd47c59696482828533e7
ricsix19 <richardladanyi30@gmail.com>
2025-05-07 15:31:11 +0200

    Reader and Book Controller implemented, documentation will be added later.

---
+++ LibraryAPI/Controllers/LoanController.cs
@@ -0,0 +1,72 @@
+﻿using LibraryAPI.Data;
+using LibraryAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LoanController: ControllerBase
+{
+    private readonly LibraryDB _context;
+
+    public LoanController(LibraryDB context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Loan>>> GetLoan()
+    {
+        return await _context.Loans.ToListAsync();
+    }
+
+    [HttpGet("reader/{readerId}")]
+    public async Task<ActionResult<IEnumerable<Loan>>> GetLoanByReaderId(int readerId)
+    {
+        return await _context.Loans.Where(l => l.UserId == readerId).ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Loan>> PostLoan(Loan loan)
+    {
+        if (loan.LoanDate.Date < DateTime.Now.Date)
+        {
+            return BadRequest("Loan out date cannot be earlier than today");
+        }
+
+        if (loan.LoanReturnDate <= loan.LoanDate)
+        {
+            return BadRequest("Loan return date cannot be earlier than Loan out date");
+        }
+
+        var ReaderExist = await _context.Readers.AnyAsync(r => r.Id == loan.UserId);
+        var BookExist = await _context.Books.AnyAsync(b => b.Id == loan.BookId);
+
+        if (!ReaderExist || !BookExist)
+        {
+            return BadRequest("Reader or Book does not exist in the database!");
+        }
+        
+        _context.Loans.Add(loan);
+        await _context.SaveChangesAsync();
+        
+        return CreatedAtAction(nameof(GetLoan), new { loanId = loan.Id }, loan);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteLoan(int id)
+    {
+        var loan = await _context.Loans.FindAsync(id);
+        if (loan == null)
+        {
+            return NotFound();
+        }
+        
+        _context.Loans.Remove(loan);
+        await _context.SaveChangesAsync();
+        
+        return NoContent();
+    }
+}
---
+++ LibraryAPI/Controllers/ReaderController.cs
@@ -0,0 +1,86 @@
+﻿using LibraryAPI.Data;
+using LibraryAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ReaderController : ControllerBase
+{
+    private readonly LibraryDB _context;
+
+    public ReaderController(LibraryDB context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Reader>>> GetReaders()
+    {
+        return await _context.Readers.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Reader>> GetReader(int id)
+    {
+        var reader = await _context.Readers.FindAsync(id);
+        if (reader == null)
+        {
+            return NotFound();
+        }
+        
+        return reader;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Reader>> PostReader(Reader reader)
+    {
+        _context.Readers.Add(reader);
+        await _context.SaveChangesAsync();
+        
+        return CreatedAtAction(nameof(GetReader), new { id = reader.Id }, reader);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutReader(int id, Reader reader)
+    {
+        if (id != reader.Id)
+        {
+            return BadRequest();
+        }
+        
+        _context.Entry(reader).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!_context.Readers.Any(r => r.Id == id))
+            {
+                return NotFound();
+            }
+            throw;
+        }
+        
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteReader(int id)
+    {
+        var reader = await _context.Readers.FindAsync(id);
+        if (reader == null)
+        {
+            return NotFound();
+        }
+        
+        _context.Readers.Remove(reader);
+        await _context.SaveChangesAsync();
+        
+        return NoContent();
+    }
+}
