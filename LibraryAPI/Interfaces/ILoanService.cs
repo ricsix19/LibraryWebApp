5780496d76c1715dfdedd1d8739c115375a8562f
ricsix19 <ricsix19@mailbox.unideb.hu>
2025-05-24 15:39:03 +0200

    feat(api): Created and half implemented the Reader and Loan service interfaces, will work on it later.

---
+++ LibraryAPI/Interfaces/ILoanService.cs
@@ -0,0 +1,17 @@
+﻿using LibraryApp.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Interfaces;
+
+public interface ILoanService
+{
+    Task<List<Loan>> GetAllLoansAsync();
+    
+    Task<Loan?> GetLoanAsync(int id);
+    
+    Task<ActionResult<Reader?>> AddLoanAsync(Loan? loan);
+    
+    Task UpdateLoanAsync(int id, Loan? loan);
+    
+    Task<ActionResult<Loan?>> DeleteLoanAsync(int id);
+}
---
+++ LibraryAPI/Interfaces/IReaderService.cs
@@ -0,0 +1,17 @@
+﻿using LibraryApp.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Interfaces;
+
+public interface IReaderService
+{
+    Task<List<Reader>> GetReadersAsync();
+    
+    Task<Reader?> GetReaderAsync(int id);
+    
+    Task<ActionResult<Reader?>> AddReaderAsync(Reader? reader);
+    
+    Task UpdateReaderAsync(int id, Reader? reader);
+    
+    Task<ActionResult<Reader?>> DeleteReaderAsync(int id);
+}
