48eeed8c2a2f20a45cf0dfdd464ebf270179cc34
ricsix19 <ricsix19@mailbox.unideb.hu>
2025-05-25 16:49:54 +0200

    feat(test): implemented test database method for readerservicetest class.

--- LibraryAPI.Tests/ReaderServiceTest.cs
+++ LibraryAPI.Tests/ReaderServiceTest.cs
@@ -1,6 +1,18 @@
-﻿namespace LibraryAPI.Tests;
+﻿using LibraryAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Tests;
 
 public class ReaderServiceTest
 {
+    private LibraryDB GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<LibraryDB>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+        
+        return new LibraryDB(options);
+    }
+    
     
 }
