cd92d37eb55f4ec1fc1a562790fbc67108d438fa
ricsix19 <ricsix19@mailbox.unideb.hu>
2025-05-25 16:41:47 +0200

    feat(test): Implemented InMemoryDbContext, so each test uses a separate clean database.

--- LibraryAPI.Tests/LoanServiceTest.cs
+++ LibraryAPI.Tests/LoanServiceTest.cs
@@ -1,5 +1,17 @@
-﻿namespace LibraryAPI.Tests;
+﻿using LibraryAPI.Data;
+using LibraryAPI.Services;
+using LibraryApp.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Tests;
 
 public class LoanServiceTest
 {
+    private LibraryDB GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<LibraryDB>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+        return new LibraryDB(options);
+    }
 }
