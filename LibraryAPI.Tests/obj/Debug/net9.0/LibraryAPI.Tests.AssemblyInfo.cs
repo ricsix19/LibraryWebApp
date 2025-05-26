b4c1d79d94364d8fbeec1227e6703ef45eb58859
ricsix19 <richardladanyi30@gmail.com>
2025-05-26 20:55:02 +0200

    fix: added .gitignore file

--- LibraryAPI.Tests/obj/Debug/net9.0/LibraryAPI.Tests.AssemblyInfo.cs
+++ LibraryAPI.Tests/obj/Debug/net9.0/LibraryAPI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a197f9fe1da1f08b8765610181b63e6bbdeecb1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a00abd81b7d1866638d3195567a9fc702cd4068")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LibraryWebApp/obj/Debug/net9.0/LibraryWebApp.AssemblyInfo.cs
+++ LibraryWebApp/obj/Debug/net9.0/LibraryWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c58819e422b14b088470eceedd422e114d7a4f6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a00abd81b7d1866638d3195567a9fc702cd4068")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
